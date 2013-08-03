cbbfb26335b900cf3fda996d170fea1fb1847bca
B. Estrade <estrabd@gmail.com>
2013-08-03 13:15:34 -0500

    organizing configs

--- configure-redis-server.sh
+++ configure-redis-server.sh
@@ -6,4 +6,6 @@ echo "bind 192.168.3.2" >> /etc/redis/redis.conf
 /etc/init.d/redis-server stop
 /etc/init.d/redis-server start
 
-sudo -u vagrant whoami
+mkdir /home/vagrant/.ssh
+cp /vagrant/keys/* /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
