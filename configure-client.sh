7ffd2c7c3f2a7392c21770d806d64f2a09559fe3
B. Estrade <estrabd@gmail.com>
2013-08-05 15:35:54 -0500

    updated configure-clients

--- configure-client.sh
+++ configure-client.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+apt-get update
 apt-get -y install libexecption-class-perl
 apt-get -y install libclass-accessor-perl
 apt-get -y install libredis-perl
-apt-get -y vim
+apt-get -y install perltidy
+apt-get -y install vim
--- lib/Queue.pm
+++ lib/Queue.pm
@@ -5,50 +5,45 @@ use warnings;
 use Data::Dumper ();
 
 use parent q{Redis};
-use Exception::Class (
-  'X::Fatal',
-  'X::CantConnect' => { isa => 'X::Fatal' },
-);
+use Exception::Class ( 'X::Fatal', 'X::CantConnect' => { isa => 'X::Fatal' }, );
 
-sub new  {
-  my $pkg = shift;
-  my $params = shift;
+sub new {
+    my $pkg    = shift;
+    my $params = shift;
 
-  my $self = {
-      name => $params->{name} || q{myqueue},
-  };
+    my $self = { name => $params->{name} || q{myqueue}, };
 
-  bless $self, $pkg;
+    bless $self, $pkg;
 
-  my $ok = eval {
-    $self->{redis} = Redis->new( server => q{192.168.3.2:6379} )
-      or throw X::CantConnect;
-  };
+    my $ok = eval {
+        $self->{redis} = Redis->new( server => q{192.168.3.2:6379} )
+          or throw X::CantConnect;
+    };
 
-  return $self;
+    return $self;
 }
 
 sub submit_task {
-  my $self = shift;
-  my $payload = shift;
-  return $self->{redis}->rpush( $self->{name}, $payload );
+    my $self    = shift;
+    my $payload = shift;
+    return $self->{redis}->rpush( $self->{name}, $payload );
 }
 
 sub bget_task {
-  my $self = shift;
-  my $timeout = shift // 1;
-  my ($list, $payload) = $self->{redis}->blpop( $self->{name}, $timeout );
-  return $payload;
+    my $self = shift;
+    my $timeout = shift // 1;
+    my ( $list, $payload ) = $self->{redis}->blpop( $self->{name}, $timeout );
+    return $payload;
 }
 
 package main;
 
-my $queue = Queue->new( { name => q{fartbox} });
+my $queue = Queue->new( { name => q{fartbox} } );
 
-for my $i (1..10) {
-  $queue->submit_task($i);
-  my $task = $queue->bget_task(0);
-  print qq{$task\n}; 
+for my $i ( 1 .. 10 ) {
+    $queue->submit_task($i);
+    my $task = $queue->bget_task(0);
+    print qq{$task\n};
 }
 
 1;
