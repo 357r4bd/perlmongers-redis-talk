e3b71b2d85c959407750bc7416db942842694bcf
B. Estrade <estrabd@gmail.com>
2013-08-07 17:23:35 +0000

    cleaned up Message.pm, based it off of Task.pm but overrode new and _encode; _decode is generic

--- lib/Message.pm
+++ lib/Message.pm
@@ -2,34 +2,28 @@ package Message;
 
 use strict;
 use warnings;
-use JSON::XS ();
-use Digest::SHA qw/sha512_hex/;
 
-use Exception::Class (
-    'X::Fatal',
-    'X::InvalidPayload' => { isa => 'X::Fatal' },
-    'X::InvalidJSON'    => { isa => 'X::Fatal' },
-    'X::InvalidField'   => { isa => 'X::Fatal' },
-);
+use parent q{Task};
+
+use Exception::Class ( 'X::MissingHeader' => { isa => 'X::Fatal' }, );
 
 sub new {
     my $pkg     = shift;
     my $payload = shift;
     my $headers = shift;
-    if (    ref $payload ne q{SCALAR}
-        and ref $payload ne q{HASH}
-        and ref $payload ne q{ARRAY} )
-    {
-        throw X::InvalidPayload(
-            "Payload must be a scalar, hash ref, array ref");
-    }
+
+    # only basic validation
+    throw X::MissingPayload if not $payload or ref $payload ne q{HASH};
+
+    # initialize fields
     my $self = {
         id => sha512_hex( $pkg, rand, rand, rand ),
-        to   => $headers->{to}   // throw X::InvalidHeader('to not defined'),
-        from => $headers->{from} // throw X::InvalidHeader('from not defined'),
-        replyto => $headers->{replyto} // $headers->{from},
         type    => $pkg,
-        payload => $payload            // undef,
+        payload => $payload,
+        # header info can be set if defined as second hash ref with 'to', 'from', & optional 'replyto' field
+        to      => $headers->{to}      // undef,
+        from    => $headers->{from}    // undef,
+        replyto => $headers->{replyto} // undef,
     };
     bless $self, $pkg;
     return $self;
@@ -42,26 +36,15 @@ sub _encode_task {
     # use important hash keys
     return JSON::XS::encode_json(
         {
+            id      => $self->{id},
             type    => $self->{type},
             payload => $self->{payload},
-            id      => $self->{id},
-            to      => $self->{to},
-            from    => $self->{from},
-            replyto => $self->{replytl},
+            # build headers, throw fatal exception if 'to' or 'from' is missing
+            to => $self->{to} // throw X::MissingHeader(q{'to' field not set}),
+            from => $self->{from} // throw X::MissingHeader(q{'from' field not set}),
+            replyto => $self->{replyto} // $self->{from},
         }
     );
-}
-
-sub _decode_task {
-    my $self = shift;
-    my $json = shift;
-    local $@;
-    my $task_ref = eval { JSON::XS::decode_json($json) }
-      or throw X::InvalidJSON($@);
-
-    # create hash_ref, then bless it to embue properties of type $pkg
-    bless $task_ref, $task_ref->{type};
-    return $task_ref;
 }
 
 1;
--- lib/Task.pm
+++ lib/Task.pm
@@ -3,45 +3,52 @@ package Task;
 use strict;
 use warnings;
 use JSON::XS ();
+use Digest::SHA qw/sha512_hex/;
 
 use Exception::Class (
     'X::Fatal',
-    'X::InvalidPayload' => { isa => 'X::Fatal' },
     'X::InvalidJSON'    => { isa => 'X::Fatal' },
+    'X::MissingPayload' => { isa => 'X::Fatal' },
 );
 
 sub new {
     my $pkg     = shift;
     my $payload = shift;
-    if (    ref $payload ne q{SCALAR}
-        and ref $payload ne q{HASH}
-        and ref $payload ne q{ARRAY} )
-    {
-        throw X::InvalidPayload(
-            "Payload must be a scalar, hash ref, array ref");
-    }
+
+    # only basic validation
+    throw X::MissingPayload if not $payload or ref $payload ne q{HASH};
+
+    # initialize fields
     my $self = {
+        id => sha512_hex( $pkg, rand, rand, rand ),
         type    => $pkg,
-        payload => $payload // undef,
+        payload => $payload, 
     };
+
     bless $self, $pkg;
     return $self;
 }
 
-#
 sub _encode_task {
     my $self = shift;
+
     # use important hash keys
     return JSON::XS::encode_json(
-        { type => $self->{type}, payload => $self->{payload} } );
+        {
+            id      => $self->{id},
+            type    => $self->{type},
+            payload => $self->{payload}
+        }
+    );
 }
 
 sub _decode_task {
     my $self = shift;
     my $json = shift;
     local $@;
-    my $task_ref = eval { JSON::XS::decode_json( $json ) }
+    my $task_ref = eval { JSON::XS::decode_json($json) }
       or throw X::InvalidJSON($@);
+
     # create hash_ref, then bless it to embue properties of type $pkg
     bless $task_ref, $task_ref->{type};
     return $task_ref;
