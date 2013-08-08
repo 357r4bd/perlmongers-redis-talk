e6bac79c533fc5aaa1d72065db7bd9853187934c
vagrant <vagrant@precise64.(none)>
2013-08-08 04:31:06 +0000

    added synchronous example, emulating private queues and messages

--- examples/sync-consumer-producer/consumer.pl
+++ examples/sync-consumer-producer/consumer.pl
@@ -4,10 +4,18 @@ use strict;
 use warnings;
 
 use Queue::WithTasks;
+use Digest::SHA qw/sha512_hex/;
+
+$|++;
 
 my $qclient = Queue::WithTasks->new( { name => q{workq} });
+
 while (my $task = $qclient->bget_task(30)) {
-  print $task->_encode_task(), qq{\n}; 
+  print qq{Received producer task ... sending ACK\n}; 
+  my $ack = Task->new({ inreplyto => $task->{id}, msg => q{ACK} });
+  # set up client to send ack to producer's private queue
+  my $qack = Queue::WithTasks->new({ name => $task->{payload}->{replyto} });  
+  $qack->submit_task($ack);
 }
 
 print qq{Consumer timed out\n};
--- examples/sync-consumer-producer/producer.pl
+++ examples/sync-consumer-producer/producer.pl
@@ -5,11 +5,22 @@ use warnings;
 
 use Queue::WithTasks;
 use Task;
+use Digest::SHA qw/sha512_hex/;
+
+$|++;
 
 my $qclient = Queue::WithTasks->new( { name => q{workq} });
+my $qlisten  = Queue::WithTasks->new( { name => sha512_hex(rand, rand, rand, $qclient->{redis}->incr(q{producer})) } );
 
 for my $i (1 .. 1000) {
-  my $task = Task->new({ life => 42, id => $i });
+  # add 'replyto' so consumer knows what private queue to submit ACK to
+  my $task = Task->new({ life => 42, id => $i, replyto => $qlisten->{name} });
   $qclient->submit_task($task);
   print qq{Submitted $i\n};
+  my $ack = $qlisten->bget_task(5);
+  if (not $ack) {
+    print qq{ACK not received from consumer\n};
+    exit;
+  }
+  print qq{Got ACK from consumer\n};
 }
